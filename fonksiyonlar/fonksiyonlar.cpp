7791cb97fad826d904f8cd2496f969025491f10e
Caner <Caner@DESKTOP-V48FJUH>
2023-12-24 17:11:15 +0300

    Proje dosyası ekle.

---
+++ fonksiyonlar/fonksiyonlar.cpp
@@ -0,0 +1,198 @@
+﻿
+
+//Caner Erdoğdu
+
+//#include <iostream>
+//#include <string>
+
+//using namespace std;
+
+//void selamla(std::string isim) {
+	//cout << "merhaba, " << isim << endl;	isim girildigi zaman selamlar methot gibi
+//}
+//int main() {
+	//std::string name;
+	//cout << "isminizi giriniz: ";
+	//cin >> name;
+
+	//selamla(name);
+
+//}
+
+
+
+
+
+
+//string isim(string a) {		yazı yazdırma şekli
+//	return a;
+//}
+//int main() {
+//	string x = "caner erdogdu";
+//	
+//
+//	cout << "isim ve soyisimin birlesimi: " << isim(x);
+//}
+
+
+
+
+
+
+//int enBuyuk(int a, int b,int c) {		en buyuk sayiyi bulur
+	//int max = a;
+	//if (b > max) {
+		//max = b;
+		
+	//}
+	//if (c > max) {
+	//	max = c;
+	//}
+	//return max;
+//}
+//int main() {
+	//int s1, s2, s3;
+	//cout << "sayi giriniz: ";
+	//cin >> s1;
+	//cout << "sayi giriniz: ";
+	//cin >> s2;
+	//cout << "sayi giriniz: ";
+	//cin >> s3;
+
+	//cout << "enbuyuk sayi: " << enBuyuk(s1, s2, s3) << endl;
+//
+//}
+
+
+
+
+	
+//int topla(int a, int b) {		toplama islemi yapar
+//	return a + b;
+//}
+//int main() {
+//	int s1 = 5;
+//	int s2 = 5;
+//
+//	cout << "sonuc: " << topla(s1 , s2)<<endl;
+//	
+//}
+
+
+
+
+
+//int topla(int a, int b) {		kullanıcıdan veri alınarak toplar
+//	return a + b;
+//}
+//int main() {
+//	int s1, s2;
+//	cout << "sayi gir: ";
+//	cin >> s1;
+//	cout << "sayi gir: ";
+//	cin >> s2;
+//
+//	cout << "sonuc: " << topla(s1, s2)<<endl;
+//}
+
+
+
+
+
+
+//int topla(int a, int b) {		4 işlem yapabilen hesap makinesi
+//	return a + b;
+//}
+//int cikar(int a, int b) {
+//	return a - b;
+//}
+//int carp(int a, int b) {
+//	return a * b;
+//}
+//int bol(int a, int b) {
+//	return a / b;
+//}
+//int main() {
+//	int s1, s2;
+//	char secim;
+//
+//	cout << "secim yapiniz '+','-','*','/'";
+//	cin >> secim;
+//	cout << "sayi gir: ";
+//	cin >> s1;
+//	cout << "sayi gir: ";
+//	cin >> s2;
+//	int sonuc;
+//	switch (secim) {
+//
+//	case '+':
+//		sonuc = s1 + s2;
+//	break;
+//
+//	case '-':
+//	sonuc = s1 - s2;
+//	break;
+//
+//	case '*':
+//	sonuc = s1 * s2;
+//	break;
+//
+//	case '/':
+//	sonuc = s1 / s2;
+//	break;
+//
+//
+//
+//	}
+//	cout << "sonuc: " << sonuc;
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//double alan(double a, double b) {		bir sayinin alanı ve cevresini hesaplar
+//	return a + b;
+//}
+//double cevre(double a, double b) {
+//	return a * b;
+//}
+//int main() {
+//	int s1, s2;
+//	cout << "sayi gir: ";
+//	cin >> s1;
+//	cout << "sayi gir: ";
+//	cin >> s2;
+//
+//	cout << "sayinin alani: " << alan(s1, s2) << endl;
+//	cout << "sayinin cevresi: " << cevre(s1, s2) << endl;
+//
+//	return 0;
+//
+//}
+
+
+
+
+
+
+
+//double karekokBul(double s1) {	sayinin karekokunu bulur
+//	return sqrt(s1);
+//}
+//int main() {
+//	double s2;
+//
+//	cout << "sayi gir: ";
+//	cin >> s2;
+//
+//	cout << "sayinin karekoku : " << karekokBul(s2);
+//}
+
+
+
+
